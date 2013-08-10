01a7cda2a9fae107ee488218a3593245f7280d47
Stuart Garvagh <sgarvagh@uoguelph.ca>
2013-08-10 17:00:40 -0400

    add source test

---
+++ test/integration/source_flow_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class TopicsFlowTest < ActionDispatch::IntegrationTest
+  test "users can add sources" do
+  	setup_signed_up_user
+  	visit topic_path(Topic.first)
+  	find('.btn-new-source').click_button('+')
+
+  	assert page.has_content? "Name"
+  	assert page.has_content? "Address"
+  	
+  	fill_in "source[name]", with: "Twitter"
+    fill_in "source[url]", with: "www.twitter.com"
+    click_button "Submit resource"
+
+    assert_equal current_path, topic_path(Topic.first)
+    assert page.has_content? "Thanks for submitting a resource!"
+  end
+end
