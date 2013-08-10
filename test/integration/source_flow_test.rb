e2f692879fcc248916c1c352a1bf8a5c4e0ee02d
Stuart Garvagh <sgarvagh@uoguelph.ca>
2013-08-10 17:06:19 -0400

    fail and pass add source int test

--- test/integration/source_flow_test.rb
+++ test/integration/source_flow_test.rb
@@ -9,6 +9,9 @@ class TopicsFlowTest < ActionDispatch::IntegrationTest
   	assert page.has_content? "Name"
   	assert page.has_content? "Address"
   	
+  	click_button "Submit resource"
+  	assert page.has_content? "Name can't be blank"
+
   	fill_in "source[name]", with: "Twitter"
     fill_in "source[url]", with: "www.twitter.com"
     click_button "Submit resource"
